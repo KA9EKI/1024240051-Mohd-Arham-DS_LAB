ff331aac9c33cf933cf4112d073545dc4e668f49
KA9EKI <86768686+KA9EKI@users.noreply.github.com>
2025-08-31 23:19:06 +0530

    Add files via upload

---
+++ Assignment-3/qn-3.cpp
@@ -0,0 +1,38 @@
+// Write a program that checks if an expression has balanced parentheses.
+
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+bool checkParenthesis(string exp){
+    stack <char> s;
+    for(int i=0;i<exp.length();i++){
+        if(exp[i]=='('||exp[i]=='{'||exp[i]=='['){
+            s.push(exp[i]);
+        }else if((exp[i]==')'||exp[i]=='}'||exp[i]==']')&& !s.empty()){
+            if((s.top()=='('&&exp[i]==')')||(s.top()=='{'&&exp[i]=='}')||(s.top()=='['&&exp[i]==']')){
+                s.pop();
+            }else{
+                return false;
+            }
+        }
+    }
+    if(s.empty()){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+int main()
+{
+    string exp ="{}({)[]";
+    if(checkParenthesis(exp)){
+        cout<<"the expression is valid";
+    }else{
+        cout<<"the expression is invalid";
+    }
+    
+    return 0;
+}
